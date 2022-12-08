9b46520b4ab5746b2835ad28d17e57b390752ce8
crazyetiii <50905019+crazyetiii@users.noreply.github.com>
2022-12-08 09:16:13 +0800

    v1.1
    1.更新vs的输出
    2.添加.idea目录

--- gen_git_ignore.py
+++ gen_git_ignore.py
@@ -2,11 +2,11 @@ import os
 import sys
 
 GIT_IGNORE_FILE_NAME = '.gitignore'
-VERSION = 'v1.0'
+VERSION = 'v1.1'
 
 
 def get_version():
-    return 'generate .gitignore file('+VERSION+')'
+    return 'generate .gitignore file(' + VERSION + ')'
 
 
 def gen_svn_dir_str():
@@ -14,7 +14,14 @@ def gen_svn_dir_str():
 
 
 def gen_visul_studio():
-    return "# visual studio\nDebug\nRelease\n\n"
+    return "# visual studio\n" + \
+           ".vs\n" + \
+           "**/Debug/*\n" + \
+           "**/Release/*\n\n"
+
+
+def gen_ideas():
+    return ".idea\n"
 
 
 def write_2_file_path(git_ignore_path):
@@ -22,6 +29,7 @@ def write_2_file_path(git_ignore_path):
     with open(git_ignore_path, 'w', encoding='utf-8', errors='ignore') as fw:
         fw.write(gen_svn_dir_str())
         fw.write(gen_visul_studio())
+        fw.write(gen_ideas())
 
 
 def get_end_str():
@@ -30,5 +38,5 @@ def get_end_str():
 
 if __name__ == '__main__':
     print(get_version())
-    write_2_file_path(os.getcwd()+"\\"+GIT_IGNORE_FILE_NAME)
+    write_2_file_path(os.getcwd() + "\\" + GIT_IGNORE_FILE_NAME)
     print(get_end_str())
